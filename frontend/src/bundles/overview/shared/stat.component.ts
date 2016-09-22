dae66f5aa5f6222039e65c8626303ab735f5ff23
xCausxn <xcausxn@hotmail.co.uk>
2016-09-22 22:24:15 +0100

    Added live-stat component

---
+++ frontend/src/bundles/overview/shared/stat.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/startWith';
+
+import { BeamApiService } from '../../main/services';
+
+
+@Component({
+    selector: 'live-stat',
+    template: `
+        {{statistic$ | async}}
+    `
+})
+export class StatComponent implements OnInit {
+
+
+    @Input() endpoint: string = '/v1/channels';
+    @Input() params: Object = {};
+
+    public statistic$;
+
+    constructor(private beamApi: BeamApiService) {}
+
+    ngOnInit() { 
+        const defaults = {
+            limit: 1
+        };
+        this.statistic$ = Observable.interval(5000).startWith(0)
+            .mergeMap(() => this.beamApi.getEndpoint(`${this.endpoint}`, Object.assign(defaults, this.params)))
+            .map(res => res.headers.get('x-total-count') || res.json().length);
+    }
+
+}
