8a0ef40e883b258d1d6a5f5be769d239776e53e4
xCausxn <xcausxn@hotmail.co.uk>
2016-09-23 21:35:23 +0100

    remove comnt

--- frontend/src/bundles/ranking/ranking.module.ts
+++ frontend/src/bundles/ranking/ranking.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { rankingRouting, rankingRoutingProviders } from './ranking.routing';
+
 import { RankingComponent } from './ranking.component';
 
 @NgModule({
