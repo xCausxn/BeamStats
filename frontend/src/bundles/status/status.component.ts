e84e8130dffdc79809ab2c933c517947be269511
xCausxn <xCausxn@hotmail.co.uk>
2016-10-14 23:00:05 +0100

    Set interval on Status updates

--- frontend/src/bundles/status/status.component.ts
+++ frontend/src/bundles/status/status.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { BeamStatsApiService } from "../main/services";
 
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
-
+import 'rxjs/add/operator/startWith';
 
 @Component({
     selector: 'beamstats-status',
@@ -19,7 +22,8 @@ export class StatusComponent implements OnInit {
     }
 
     ngOnInit() { 
-        this.status$ = this.bstatsApi.getStats()
+        this.status$ = Observable.interval(20000).startWith(0)
+            .mergeMap(() => this.bstatsApi.getStats())
             .do((res: {ingests: any[]}) => res.ingests = res.ingests.sort((a, b) => a['name'].localeCompare(b['name'])))
             .publishReplay(1)
             .refCount()
